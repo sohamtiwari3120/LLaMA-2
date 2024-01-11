d22b08316f2c12956d0764286d3c7ff97eac58d6
sohamtiwari3120 <soham.tiwari800@gmail.com>
2024-01-11 22:39:18 +0530

    feat: completed feedforward layer with swiglu

--- model.py
+++ model.py
@@ -154,7 +154,27 @@ class SelfAttention(nn.Module):
         out = out.transpose(1, 2).contiguous().view(batch_size, seq_len, -1)
         # (B, seq_len, dim) -> (B, seq_len, dim)
         return self.wo(out)
+    
+class FeedForward(nn.Module):
+    def __init__(self, args: ModelArgs) -> None:
+        super().__init__()
+        hidden_dim = 4 * args.dim
+        hidden_dim = int(2 * hidden_dim / 3)
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(args.ffn_dim_multiplier * hidden_dim)
+        # round the hidden dim to th nearest (next) multiple of the args.multiple_of param
+        hidden_dim = args.multiple_of * ((hidden_dim + args.multiple_of - 1)//args.multiple_of)
 
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        swish = F.silu(self.w1(x))
+        x_V = self.w3(x)
+        x = swish * x_V
+        x = self.w2(x)
+        return x
 
 class TransformerBlock(nn.Module):
     def __init__(self, args: ModelArgs) -> None:
